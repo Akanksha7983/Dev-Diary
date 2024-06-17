fea7a89d4aa366ecde73e7ce893ca6273c30a335
Akanksha Chaudhary <akanksha7983@gmail.com>
2024-06-17 13:07:38 +0530

    add comments section to admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,11 @@
 import {Sidebar} from 'flowbite-react'
-import {HiArrowSmRight, HiDocumentText,  HiOutlineUserGroup, HiUser} from 'react-icons/hi'
+import {
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+} from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -60,14 +66,26 @@ export default function DashSidebar() {
 
 {currentUser.isAdmin && (
 
+<>
 <Link to='/dashboard?tab=users'>
   <Sidebar.Item
-     active={tab === 'posts'}
-     icon={HiOutlineUserGroup}
-     as='div'>
-      Users
-     </Sidebar.Item>
+    active={tab === 'users'}
+    icon={HiOutlineUserGroup}
+    as='div'
+  >
+    Users
+  </Sidebar.Item>
 </Link>
+<Link to='/dashboard?tab=comments'>
+  <Sidebar.Item
+    active={tab === 'comments'}
+    icon={HiAnnotation}
+    as='div'
+  >
+    Comments
+  </Sidebar.Item>
+</Link>
+</>
 )}
 
                <Sidebar.Item  icon = {HiArrowSmRight} className='cursor-pointer'
@@ -77,5 +95,5 @@ export default function DashSidebar() {
             </Sidebar.ItemGroup>
          </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
